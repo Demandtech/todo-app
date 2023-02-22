69c9de5d0a9a867f6077b683abe0c5aefc0e0c0e
Demand Btc <102271199+Demandtech@users.noreply.github.com>
2023-02-22 17:58:27 +0100

    todos functionality continuation

--- app.js
+++ app.js
@@ -5,51 +5,75 @@ const todoList = document.querySelector('.todo-list')
 const todoVal = document.querySelector('.todo-val')
 const form = document.querySelector('.form')
 const checker = document.querySelector('.checker')
-const controller = document.querySelector('.todo-footer')
+const controller = document.querySelector('.controllers')
+const itemLeftEl = document.querySelector('.items-left')
+const controleBtn = document.querySelectorAll('.btn')
+const active = document.querySelector('.active')
+const clearBtn = document.querySelector('.clear-btn')
 
+//Placeholder
+let todos = [
+  {
+    text: 'Complete online javaScript course',
+    isCompleted: true,
+    id: '111',
+  },
+  {
+    text: 'Jog around the park 3x',
+    isCompleted: false,
+    id: '222',
+  },
+  {
+    text: '10 minutes meditation',
+    isCompleted: false,
+    id: '333',
+  },
+  {
+    text: 'Read for 1 hour',
+    isCompleted: false,
+    id: '444',
+  },
+  {
+    text: 'Pick up groceries',
+    isCompleted: false,
+    id: '555',
+  },
+  {
+    text: 'Complete Todo App on Frontend Mentor',
+    isCompleted: false,
+    id: '666',
+  },
+]
+
+
+//Todos Storage
+if(localStorage.getItem('todos')){
+  todos = JSON.parse(localStorage.getItem('todos'))
+}
+//theme storage
+const theme = localStorage.getItem('theme') || 'light-theme'
+html.classList.add(theme)
+
+//Theme Toggle and change toggle image 
 themeToggleBtn.addEventListener('click', () => {
   html.classList.toggle('dark-theme')
+  html.classList.toggle('light-theme')
+
 
   if (html.classList.contains('dark-theme')) {
     toggleImg.src = './images/icon-sun.svg'
   } else {
     toggleImg.src = './images/icon-moon.svg'
   }
+   const selectedTheme = html.classList.contains('dark-theme')
+     ? 'dark-theme'
+     : 'light-theme'
+   localStorage.setItem('theme', selectedTheme)
 })
 
-let todos = [
-  {
-    text: 'Complete Online Javascript Course',
-    isCompleted: true,
-    id: '1',
-  },
-  {
-    text: 'Jog around the park 3x',
-    isCompleted: false,
-    id: '2',
-  },
-  {
-    text: '10 minutes meditation',
-    isCompleted: false,
-    id: '3',
-  },
-  {
-    text: 'Read for one hour',
-    isCompleted: true,
-    id: '4',
-  },
-  {
-    text: 'Pickup groceries',
-    isCompleted: false,
-    id: '5',
-  },
-  {
-    text: 'Complete todo app on frontend mentor',
-    isCompleted: false,
-    id: '6',
-  },
-]
 
+
+//render function
 const renderList = (arr) => {
   const todosToRender = arr || todos
   todoList.innerHTML = ''
@@ -73,10 +97,18 @@ const renderList = (arr) => {
 
 renderList()
 
+//function to get todos from local storage
+const getTodos = ()=> {
+  localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+
+
+//Onsubmit to add items to array and render
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   if (todoVal.value) {
-    todos.push({
+    todos.unshift({
       text: todoVal.value,
       isCompleted: false,
       id: new Date().getTime().toString().slice(-4, -1),
@@ -86,46 +118,104 @@ form.addEventListener('submit', (e) => {
   }
   todoVal.value = ' '
   renderList()
+  getTodos()
+  getItemLeft()
 })
 
-const AddCompleted = () => {
-  document.addEventListener('click', (e) => {
-    const target = e.target.closest('.checker')
-    if (target) {
-      todos.map((todo) => {
-        if (todo.id === target.id) {
-          if (todo.isCompleted === true) {
-            todo.isCompleted = false
-          } else {
-            todo.isCompleted = true
-          }
+//add completed todo
+const AddCompleted = (e) => {
+  const target = e.target.closest('.checker')
+  if (target) {
+    todos.map((todo) => {
+      if (todo.id === target.id) {
+        if (todo.isCompleted === true) {
+          todo.isCompleted = false
+        } else {
+          todo.isCompleted = true
         }
-      })
-      renderList()
-    }
-  })
+      }
+    })
+    getItemLeft()
+    renderList()
+    getTodos()
+  }
 }
+document.addEventListener('click', AddCompleted)
 
-AddCompleted()
 
+//Filter buttons functionality 
 controller.addEventListener('click', (e) => {
   const allBtn = e.target.closest('.all-btn')
   const activeBtn = e.target.closest('.active-btn')
   const completedBtn = e.target.closest('.completed-btn')
-  const clearCompleted = e.target.closest('.clear-completed-btn')
 
-  
+  controleBtn.forEach((btn) => btn.classList.remove('active'))
+
+  //Display all activetodo List, and I removed this event listeners from check btn and close
   if (activeBtn) {
     removeEventListener('click', AddCompleted)
-   const activeTodos = todos.filter((todo) => !todo.isCompleted)
+    const activeTodos = todos.filter((todo) => !todo.isCompleted)
     renderList(activeTodos)
+    activeBtn.classList.add('active')
+    document.removeEventListener('click', AddCompleted)
+    document.removeEventListener('click', removeTodo)
+    hideClosebtn()
+
+    //Display all Completed todo List, and I removed this event listeners from check btn and close
   } else if (completedBtn) {
-    removeEventListener('click', AddCompleted)
+    document.removeEventListener('click', AddCompleted)
     const completedTodos = todos.filter((todo) => todo.isCompleted)
     renderList(completedTodos)
-  } else if (allBtn){ renderList()}
-  else if (clearCompleted){
-    todos = todos.filter(todo=> !todo.isCompleted)
+    document.removeEventListener('click', AddCompleted)
+    document.removeEventListener('click', removeTodo)
+    completedBtn.classList.add('active')
+    hideClosebtn()
+
+    //Display All Todos List 
+  } else if (allBtn) {
     renderList(todos)
+    allBtn.classList.add('active')
+    document.addEventListener('click', AddCompleted)
+    document.addEventListener('click', removeTodo)
+    getTodos()
   }
 })
+
+//get active item length and print to the screen
+const getItemLeft = () => {
+  let itemsLeft = []
+  itemsLeft = todos.filter((todo) => {
+    return !todo.isCompleted
+  })
+
+  itemLeftEl.innerText = `${itemsLeft.length} Items left`
+}
+getItemLeft()
+
+//clear completed todo function
+const clearCompleted = () => {
+  clearBtn.addEventListener('click', () => {
+    todos = todos.filter((todo) => !todo.isCompleted)
+    renderList(todos)
+    getTodos()
+  })
+}
+clearCompleted()
+
+//remove single todos function
+const removeTodo = (e) => {
+  let click = e.target.closest('.close-btn')
+  if (!click) return
+  const removedEl = click.parentElement
+  todos = todos.filter((todo) => todo.id !== removedEl.id)
+  renderList(todos)
+  getItemLeft()
+  getTodos()
+}
+document.addEventListener('click', removeTodo)
+
+//Hide close btns when active or completed is render
+const hideClosebtn = ()=> {
+ let removeds = document.querySelectorAll('.close-btn')
+ removeds.forEach((i) => (i.style.display = 'none'))
+}
